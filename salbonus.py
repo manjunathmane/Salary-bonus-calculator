7b4d14419b5f23fe88af5644abbc1f09e68d9a59
manjunathmane <manjunathmane38@gmail.com>
2025-11-10 16:21:10 +0530

    Update salbonus.py

--- salbonus.py
+++ salbonus.py
@@ -1,29 +1,16 @@
 import sys
 if len(sys.argv) ==3:
-  script_name = sys.argv[0]
-  name = sys.argv[1]
-  rollno = sys.argv[2]
+  salary = sys.argv[0]
+  bonus = sys.argv[1]
+  total_salary = salary + bonus
   print("User provided input values:")
 
 else:
-  script_name = sys.argv[0]
-  name = "Manjunath"
-  rollno = "101"
-  print("No input given - using default values:")
-
-print("Script Name:", script_name)
-print("Student Name:", name)
-print("Roll Number:", rollno)
-
-
-salary = sys.argv[0]
-bonus = sys.argv[1]
-total_salary = salary + bonus
+  salary = float(input("Enter the employee's salary: "))
+  bonus = 0.10 * salary
+  total_salary = salary + bonus
+  print("Bonus Amount: ₹", bonus)
+  print("Total Salary: ₹", total_salary)
 
 
 
-salary = float(input("Enter the employee's salary: "))
-bonus = 0.10 * salary
-total_salary = salary + bonus
-print("Bonus Amount: ₹", bonus)
-print("Total Salary: ₹", total_salary)
